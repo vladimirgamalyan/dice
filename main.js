87a7e648ee83a3bae04838041215f5d099036de4
Vladimir Gamalyan <vladimir.gamalyan@gmail.com>
2019-05-31 17:48:43 +0700

    Change touchstart event to click

--- main.js
+++ main.js
@@ -13,13 +13,14 @@ window.onload = function() {
     }
 
     function roll() {
+        console.log('log');
         let dice = document.getElementsByClassName('dice');
         dice[0].innerHTML = getRandomDice();
         dice[1].innerHTML = getRandomDice();
     }
 
-    // let clickArea = document.getElementsByClassName('click_area')[0];
-    // clickArea.addEventListener('click', roll);
+    let clickArea = document.getElementsByClassName('click_area')[0];
+    clickArea.addEventListener('click', roll);
 
-    document.addEventListener("touchstart", roll, false);
+    //document.addEventListener("touchstart", roll, false);
 };
