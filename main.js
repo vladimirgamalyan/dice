2059b9c0e00c98abaf8805d2b3033fdb2b48b839
Vladimir Gamalyan <vladimir.gamalyan@gmail.com>
2019-05-31 15:04:14 +0700

    Fix touchstart event

--- main.js
+++ main.js
@@ -1,20 +1,24 @@
 
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-function getRandomDice() {
-    return getRandomInt(0, 3);
-}
 
 window.onload = function() {
-    document.getElementsByClassName('click_area')[0]
-        .addEventListener('click touchstart', function (event) {
-            console.log('click');
-            let dice = document.getElementsByClassName('dice');
-            dice[0].innerHTML = getRandomDice();
-            dice[1].innerHTML = getRandomDice();
-        });
+
+    function getRandomInt(min, max) {
+        min = Math.ceil(min);
+        max = Math.floor(max);
+        return Math.floor(Math.random() * (max - min + 1)) + min;
+    }
+
+    function getRandomDice() {
+        return getRandomInt(0, 3);
+    }
+
+    function roll() {
+        let dice = document.getElementsByClassName('dice');
+        dice[0].innerHTML = getRandomDice();
+        dice[1].innerHTML = getRandomDice();
+    }
+
+    let clickArea = document.getElementsByClassName('click_area')[0];
+    clickArea.addEventListener('click', roll);
+    clickArea.addEventListener('touchstart', roll);
 };
